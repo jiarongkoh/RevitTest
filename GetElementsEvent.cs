b0c542e22559eef2ba1f274933db87a1bd40281c
Phung Tuan Hoang <phungtuanhoang1996@gmail.com>
2019-08-01 16:01:19 +0700

    Added a method to find all views that contain a stair ID

--- GetElementsEvent.cs
+++ GetElementsEvent.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 
 using Autodesk.Revit;
@@ -32,15 +32,72 @@ namespace DockablePane
             {
                 info += "\n\t" + s.Name + " " + s.Id;
 
-                    //Cast as Stairs object
+                //Cast as Stairs object
                 Stairs stair = uiDoc.Document.GetElement(s.Id) as Stairs;
 
-                    info += "\n\t" + "Number of steps: " + stair.ActualRisersNumber.ToString();
-                    info += "\n\t";
-                }
+                info += "\n\t" + "Number of steps: " + stair.ActualRisersNumber.ToString();
+                info += "\n\t";
+            }
 
             //Display on TaskDialog
             TaskDialog.Show("Revit", info);
+
+            Dictionary<ElementId, List<View>> stairViewMapping = getStairViewMapping(app);
+        }
+
+        // returns mapping of stair ID to list of views that contain it
+        private Dictionary<ElementId, List<View>> getStairViewMapping(UIApplication app)
+        {
+            // Get current active document
+            UIDocument uiDoc = app.ActiveUIDocument;
+
+            Dictionary<ElementId, List<View>> stairViewMapping = new Dictionary<ElementId, List<View>>();
+
+            // Collector used to iterate through all views in document
+            FilteredElementCollector viewCollector = new FilteredElementCollector(uiDoc.Document);
+            viewCollector.OfClass(typeof(View));
+
+            // Collector used to iterate through all stairs in document
+            FilteredElementCollector stairCollector = new FilteredElementCollector(uiDoc.Document);
+            stairCollector.OfClass(typeof(Stairs));
+
+            // First iterate through all stairs
+            foreach (Stairs stair in stairCollector)
+            {
+                Debug.WriteLine("---------------");
+                Debug.WriteLine("Stair ID: " + stair.Id);
+
+                List<View> containerViews = new List<View>();
+
+                // Iterate through all views to check if that view contains stair
+                foreach (View view in viewCollector)
+                {
+                    // Get all element ids in that view
+                    try
+                    {
+                        FilteredElementCollector elementsInView = new FilteredElementCollector(uiDoc.Document, view.Id);
+
+                        // If contains our stair ID, add to stair view mapping
+                        if (elementsInView.ToElementIds().Contains(stair.Id))
+                        {
+                            containerViews.Add(view);
+                            Debug.WriteLine("---");
+                            Debug.WriteLine("View ID: " + view.Id);
+                            Debug.WriteLine("View name: " + view.Name);
+                            Debug.WriteLine("---");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e);
+                    }
+                }
+
+                // add to mapping before going to next stair
+                stairViewMapping[stair.Id] = containerViews;
+            }
+
+            return stairViewMapping;
         }
 
         public string GetName()
